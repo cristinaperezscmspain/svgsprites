67995f3404a53c036abb30d9e64b4740b55641f1
Cristina <cristina.perez@scmspain.com>
2015-03-27 14:08:06 +0100

    svg icons colors

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,26 +4,50 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        clean: ['svgbuild/output', 'images/svgbuild/output']], //removes old data
+
+        svgmin: { //minimize SVG files
+            options: {
+                plugins: [
+                    { removeViewBox: false },
+                    { removeUselessStrokeAndFill: false }
+                ]
+            },
+            dist: {
+                expand: true,
+                cwd: 'images/svg',
+                src: ['*.svg'],
+                dest: 'images/svgmin',
+                ext: '.colors-light-danger-success.svg'
+            }
+        },
+
         grunticon: {
             myIcons: {
                 files: [{
                     expand: true,
-                    cwd: 'images/svg',
-                    src: ['*.svg', '*.png'],
+                    cwd: 'images/svgmin',
+                    src: ['*.svg'],
                     dest: "images/svgbuild"
                 }],
                 options: {
-
+                    colors: {
+                        light: '#ccc',
+                        danger: '#ed3921',
+                        success: '#8DC63F'
+                    }
                 }
             }
-        }     
+        },    
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-svgmin');
     grunt.loadNpmTasks('grunt-grunticon');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['grunticon:myIcons']);
+    grunt.registerTask('default', ['clean', 'svgmin', 'grunticon:myIcons']);
 
 };
